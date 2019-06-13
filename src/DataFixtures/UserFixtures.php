1afa03d65c45c918c1c082c31624f9571e1d9805
Tadas Janca <tadasjanca@gmail.com>
2019-06-13 14:10:31 +0300

    UserFixtures updated with logical data from Api

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\API\RandomUserApi;
 use App\Entity\User;
 use App\Helpers\RandomDate;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -27,12 +28,16 @@ class UserFixtures extends Fixture
 
         for ($i = 0; $i < 10; $i++){
             $createdDate = $randomDates->get(15);
+
+            $userApi = RandomUserApi::get();
+
             $user = new User();
             $user
-                ->setUsername('username' . $i)
-                ->setFirstName('Firstname' . $i)
-                ->setLastName('Lastname' .$i)
-                ->setEmail('email' . $i . '@email.lt')
+                ->setUsername($userApi->login->username)
+                ->setFirstName(ucfirst($userApi->name->first))
+                ->setLastName(ucfirst($userApi->name->last))
+                ->setEmail($userApi->email)
+                ->setProfilePicture($userApi->picture->large)
                 ->setCreatedAt($createdDate)
                 ->setPassword($this->passwordEncoder->encodePassword(
                     $user,
