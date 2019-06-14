d2f571dea16702a2f1b6b4d4e896ed4897f78968
Tadas Janca <tadasjanca@gmail.com>
2019-06-14 10:49:41 +0300

    added form for mainComment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\API\RandomJokesApi;
 use App\Entity\Comment;
+use App\Form\MainCommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
@@ -12,13 +14,32 @@ class CommentController extends AbstractController
     /**
      * @Route("/comment", name="comment")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $mainCommentForm = $this->createForm(MainCommentType::class)
+            ->handleRequest($request);
+        if ($this->mainCommentCreated($mainCommentForm)) {
+
+            return $this->redirectToRoute('comment');
+        }
         $mainComments = $this->getDoctrine()->getRepository(Comment::class)->getMainComments();
         $nestedComments = $this->getDoctrine()->getRepository(Comment::class)->getNestedComments();
         return $this->render('comment/index.html.twig', [
             'mainComments' => $mainComments,
             'nestedComments' => $nestedComments,
+            'mainCommentForm' => $mainCommentForm->createView(),
         ]);
+    }
+
+    private function mainCommentCreated($mainCommentForm)
+    {
+        if ($mainCommentForm->isSubmitted() && $mainCommentForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $newMainComment = $mainCommentForm->getData();
+            $em->persist($newMainComment);
+            $em->flush();
+
+            return true;
+        }
     }
 }
---
+++ src/Form/MainCommentType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MainCommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->setMethod('POST')
+            ->add('userId', NumberType::class, [
+                'attr' => ['hidden' => ''],
+                'label' => false
+            ])
+            ->add('text', TextType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Add a comment']
+            ])
+            ->add('createdAt', DateTimeType::class, [
+                'data' => new \DateTime(),
+                'attr' => ['hidden' => ''],
+                'widget' => 'single_text',
+                'label' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
