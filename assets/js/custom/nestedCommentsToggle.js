48f854fb6b4b8acfaa9b3e011d62adc94f72ab5c
Tadas Janca <tadasjanca@gmail.com>
2019-06-15 15:38:11 +0300

    added toggle for each set of nestedComments

--- assets/js/custom/nestedCommentsToggle.js
+++ assets/js/custom/nestedCommentsToggle.js
@@ -1,24 +1,30 @@
 const nestedCommentsToggle = (function () {
-    let _nestedComments = document.getElementById("nestedComments-896")
-    let _commentsToggler = document.getElementById('commentsToggler-896')
 
-    function _toggle(event)
+    let _allTogglers = document.getElementsByClassName('commentsToggler');
+    let _countMainComments =_allTogglers.length;
+    let _allNestedComments = [];
+    for (let i = 0; i < _countMainComments; i++) {
+        let mainCommentId = _allTogglers[i].getAttribute("data-mainComment");
+        _allNestedComments[i] = document.getElementById("nestedComments-" + mainCommentId);
+    }
+
+    function toggle()
     {
-        if(_nestedComments.hasAttribute('hidden')) {
-            _nestedComments.removeAttribute("hidden");
-        } else {
-            let _hidden = document.createAttribute("hidden");
-            _nestedComments.setAttributeNode(_hidden);
+        for (let i = 0; i < _countMainComments; i++) {
+            _allTogglers[i].addEventListener('click', function () {
+                if(_allNestedComments[i].hasAttribute('hidden')) {
+                    _allNestedComments[i].removeAttribute("hidden");
+                } else {
+                    let _hidden = document.createAttribute("hidden");
+                    _allNestedComments[i].setAttributeNode(_hidden);
+                }
+            });
         }
     }
 
-    function init()
-    {
-        _commentsToggler.addEventListener('click', _toggle);
-    }
-
     return {
-        init: init()
+        toggle: toggle()
     }
 })();
+
 export default nestedCommentsToggle;
