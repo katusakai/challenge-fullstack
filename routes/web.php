6328e6612ab4c99525270fee1979a11345633b89
TadasJanca <tadasjanca@gmail.com>
2019-07-02 17:10:01 +0300

    google auth and google login backend added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/redirect', 'Auth\LoginController@redirectToProvider')->name('google.login');
+Route::get('/callback', 'Auth\LoginController@handleProviderCallback');
