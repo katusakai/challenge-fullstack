7e9ade3d232199717971eb8e1ad468ce6ad24f2e
TadasJanca <tadasjanca@gmail.com>
2019-07-03 23:15:47 +0300

    added google login link to nav bar, removed /home redirects from various controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Aggregators\Comments;
-use App\Helpers\CommentsId;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,9 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
+                                <a class="nav-link" href="{{ route('google.login') }}">Sign in with Google</a>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
